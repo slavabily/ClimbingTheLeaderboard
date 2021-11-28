650cdd889a0ae1482114003dc72b5941fe07c0d5
slava bily <slavabily@me.com>
2021-11-28 15:32:38 +0200

    Initial Commit

---
+++ Climbing the Leaderboard  Climbing the Leaderboard  Climbing the Leaderboard  ClimbingTheLeaderboard/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Climbing the Leaderboard  Climbing the Leaderboard  Climbing the Leaderboard  ClimbingTheLeaderboard
+//
+//  Created by slava bily on 28.11.2021.
+//
+
+import Foundation
+
+print("Hello, World!")
+
