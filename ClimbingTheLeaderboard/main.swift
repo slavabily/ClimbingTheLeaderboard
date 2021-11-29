b23feb70b51048d8f1afeb91cad52b9ca0547f70
slava bily <slavabily@me.com>
2021-11-29 16:27:43 +0200

    successful run against 7 from 11 tests

--- ClimbingTheLeaderboard/main.swift
+++ ClimbingTheLeaderboard/main.swift
@@ -11,18 +11,21 @@ func climbingLeaderboard(ranked: [Int], player: [Int]) -> [Int] {
     let sortedSet = Set(ranked).sorted().reversed()
     let arrSorted = Array(sortedSet)
     print(arrSorted)
-    var mappedRanked = [(Int, Int)]()
-    var r = 0
-    for i in 0..<arrSorted.count {
-        r += 1
-        let t = (arrSorted[i], r)
-        print(t)
-        mappedRanked.append(t)
-    }
-    print(mappedRanked)
-     
-    return [0]
+    var playersRankArr = [Int]()
+    for i in player {
+        let last = arrSorted.last
+        if i < last! {
+            playersRankArr.append(arrSorted.firstIndex(of: last!)! + 2)
+        }
+        for j in 0..<arrSorted.count {
+            if i >= arrSorted[j] {
+                playersRankArr.append(j + 1)
+                break
+            }
+        }
+    } 
+    return playersRankArr
 }
 
-print(climbingLeaderboard(ranked: [100, 90, 90, 80, 75, 60], player: [50, 65, 77, 90, 102]))
+print(climbingLeaderboard(ranked: [100, 100, 50, 40, 40, 20, 10], player: [5, 25, 50, 120]))
 
